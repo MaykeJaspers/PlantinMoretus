68536e59e1bbcc49f58f972bd35c9645936fb6c1
mayke <mayke.jaspers@howest.be>
2025-01-29 10:14:13 +0100

    images

--- plantinmoretus/src/main.js
+++ plantinmoretus/src/main.js
@@ -3,3 +3,9 @@ import '../src/css/reset.css'
 import '../src/js/cursor'
 import '../src/js/popup'
 import '../src/js/hamburger'
+
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+// Register the plugin
+gsap.registerPlugin(ScrollTrigger);
